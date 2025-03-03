029706db4698cd91387e01fe89757cb0233f55ed
H1nkk <the_h1nk@mail.ru>
2025-03-03 22:21:37 +0300

    Add aggregator

--- include/table.h
+++ include/table.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "polynomial.h"
 #include <string>
+#include <vector>
 
 class Table {
 
@@ -57,4 +58,18 @@ public:
 	virtual void addPolynomial(const polynomial& pol);
 	virtual void delPolynomial(const std::string& polName);
 	virtual ~SeparateChainingHashTable() {};
+};
+
+class Aggregator {
+	std::vector<Table*> tables;
+public:
+	void selectTable(const std::string& tableName); // select table by its name: tables[0] - liar (short for linear array table - contains LinearArrTable object)
+													//							 tables[1] - lili (short for linear list table - contains LinearListTable object)
+													//							 tables[2] - ordr (short for ordered array table - contains OrderedTable object)
+													//							 tables[3] - tree (short for tree table - contains TreeTable object)
+													//							 tables[4] - opha (short for open adress hash table - contains OpenAdressHashTable object)
+													//							 tables[5] - seha (short for separate chaining hash table - contains SeparateChainingHashTable object)
+	polynomial findPolynomial(const std::string& polName); // find polynomial named polName
+	void addPolynomial(const polynomial& pol);
+	void delPolynomial(const std::string& polName);
 };
