c6715bd96d7e9af7d8ec96d10d8e623addfb3d36
Alexey Gutyansky <75169888+AlexixRugis@users.noreply.github.com>
2025-03-04 12:08:09 +0300

    finally works

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
+#include <qapplication>
+#include <qpushbutton>
 #include "test.h"
 
-int main() {
-    std::cout << sum(1, 2) << std::endl;
-    return 0;
+int main(int argc, char* argv[]) {
+
+    QApplication app(argc, argv);
+
+    QPushButton button("Click");
+    button.resize(200, 500);
+    button.show();
+
+    return app.exec();
 }
