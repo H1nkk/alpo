b061edb80f9d3e32cacee05fc308eef34a8e43f8
Alexey Gutyansky <75169888+AlexixRugis@users.noreply.github.com>
2025-03-10 20:11:37 +0300

    Create test_lexer.cpp

---
+++ test/test_lexer.cpp
@@ -0,0 +1,42 @@
+#include <gtest/gtest.h>
+#include "lexer/lexer.h"
+
+TEST(LexerTest, empty_string_returns_eof)
+{
+    ADD_FAILURE();
+}
+
+TEST(LexerTest, can_parse_numbers)
+{
+    ADD_FAILURE();
+}
+
+TEST(LexerTest, can_parse_operations)
+{
+    ADD_FAILURE();
+}
+
+TEST(LexerTest, can_parse_parenthesis)
+{
+    ADD_FAILURE();    
+}
+
+TEST(LexerTest, can_parse_identifiers)
+{
+    ADD_FAILURE();
+}
+
+TEST(LexerTest, can_perform_lexical_analysis)
+{
+    ADD_FAILURE();
+}
+
+TEST(LexerTest, can_ignore_whitespace)
+{
+    ADD_FAILURE();
+}
+
+TEST(LexerTest, throw_on_unexpected_symbol)
+{
+    ADD_FAILURE();
+}
