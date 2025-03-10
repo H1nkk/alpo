01df48584902d4c8ca85cb44995e66a6cbcb8df6
H1nkk <the_h1nk@mail.ru>
2025-03-10 20:52:39 +0300

    change tables a bit

--- include/table.h
+++ include/table.h
@@ -6,71 +6,112 @@
 class Table {
 
 public:
+
 	virtual polynomial findPolynomial(const std::string& polName) = 0; // find polynomial named polName
 	virtual void addPolynomial(const std::string& polName, const polynomial& pol) = 0;
 	virtual void delPolynomial(const std::string& polName) = 0;
+	virtual unsigned int size() = 0;
+
 	virtual ~Table() = 0 {};
 };
 
-class LinearArrTable: Table {
+class LinearArrTable: public Table {
+
 public:
+	LinearArrTable();
+
 	virtual polynomial findPolynomial(const std::string& polName) override; // find polynomial named polName
 	virtual void addPolynomial(const std::string& polName, const polynomial& pol) override;
 	virtual void delPolynomial(const std::string& polName) override;
+	virtual unsigned int size() override;
+
 	virtual ~LinearArrTable() {};
 };
 
-class LinearListTable : Table {
+class LinearListTable : public Table {
 public:
+	LinearListTable();
+
 	virtual polynomial findPolynomial(const std::string& polName) override; // find polynomial named polName
 	virtual void addPolynomial(const std::string& polName, const polynomial& pol) override;
 	virtual void delPolynomial(const std::string& polName) override;
+	virtual unsigned int size() override;
+
 	virtual ~LinearListTable() {};
 };
 
-class OrderedTable : Table {
+class OrderedTable : public Table {
 public:
+	OrderedTable();
+
 	virtual polynomial findPolynomial(const std::string& polName) override; // find polynomial named polName
 	virtual void addPolynomial(const std::string& polName, const polynomial& pol) override;
 	virtual void delPolynomial(const std::string& polName) override;
+	virtual unsigned int size() override;
+
 	virtual ~OrderedTable() {};
 };
 
-class TreeTable : Table {
+class TreeTable : public Table {
 public:
+	TreeTable();
+
 	virtual polynomial findPolynomial(const std::string& polName) override; // find polynomial named polName
 	virtual void addPolynomial(const std::string& polName, const polynomial& pol) override;
 	virtual void delPolynomial(const std::string& polName) override;
+	virtual unsigned int size() override;
+
 	virtual ~TreeTable() {};
 };
 
-class OpenAdressHashTable : Table {
+class OpenAdressHashTable : public Table {
 public:
+	OpenAdressHashTable();
+
 	virtual polynomial findPolynomial(const std::string& polName) override; // find polynomial named polName
 	virtual void addPolynomial(const std::string& polName, const polynomial& pol) override;
 	virtual void delPolynomial(const std::string& polName) override;
+	virtual unsigned int size() override;
+
 	virtual ~OpenAdressHashTable() {};
 };
 
-class SeparateChainingHashTable : Table {
+class SeparateChainingHashTable : public Table {
 public:
+	SeparateChainingHashTable();
+
 	virtual polynomial findPolynomial(const std::string& polName) override; // find polynomial named polName
 	virtual void addPolynomial(const std::string& polName, const polynomial& pol) override;
 	virtual void delPolynomial(const std::string& polName) override;
+	virtual unsigned int size() override;
+
 	virtual ~SeparateChainingHashTable() {};
 };
 
 class Aggregator {
 	std::vector<Table*> tables;
+	int currentTable;
 public:
+	Aggregator() {
+		tables.resize(6, nullptr);
+		tables[0] = new LinearArrTable();
+		tables[1] = new LinearListTable();
+		tables[2] = new OrderedTable();
+		tables[3] = new TreeTable();
+		tables[4] = new OpenAdressHashTable();
+		tables[5] = new SeparateChainingHashTable();
+		currentTable = 0;
+	}
+
 	void selectTable(const std::string& tableName) ; // select table by its name: tables[0] - liar (short for linear array table - contains LinearArrTable object)
-													//							 tables[1] - lili (short for linear list table - contains LinearListTable object)
-													//							 tables[2] - ordr (short for ordered array table - contains OrderedTable object)
-													//							 tables[3] - tree (short for tree table - contains TreeTable object)
-													//							 tables[4] - opha (short for open adress hash table - contains OpenAdressHashTable object)
-													//							 tables[5] - seha (short for separate chaining hash table - contains SeparateChainingHashTable object)
+													 //			 				  tables[1] - lili (short for linear list table - contains LinearListTable object)
+													 //		 					  tables[2] - ordr (short for ordered array table - contains OrderedTable object)
+													 //							  tables[3] - tree (short for tree table - contains TreeTable object)
+													 //							  tables[4] - opha (short for open adress hash table - contains OpenAdressHashTable object)
+													 //							  tables[5] - seha (short for separate chaining hash table - contains SeparateChainingHashTable object)
 	polynomial findPolynomial(const std::string& polName); // find polynomial named polName
 	void addPolynomial(const std::string& polName, const polynomial& pol);
 	void delPolynomial(const std::string& polName);
+
 	~Aggregator();
 };
