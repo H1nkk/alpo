02f6ab69fc217f933b217f4d25cdc86a8af58ac3
Alexey Gutyansky <75169888+AlexixRugis@users.noreply.github.com>
2025-03-10 20:47:25 +0300

    create tests for expr compiler and interpreter

---
+++ test/test_expr_compiler.cpp
@@ -0,0 +1,126 @@
+#include <gtest/gtest.h>
+#include "expression_compiler/expression_compiler.h"
+
+TEST(ExprCompilerTest, can_compile_single_number)
+{
+    ADD_FAILURE();
+}
+
+TEST(ExprCompilerTest, can_compile_identifier) {
+    ADD_FAILURE();
+}
+
+TEST(ExprCompilerTest, can_compile_single_monomial)
+{
+    ADD_FAILURE();
+}
+
+TEST(ExprCompilerTest, can_compile_sum)
+{
+    ADD_FAILURE();
+}
+
+TEST(ExprCompilerTest, can_compile_subtraction)
+{
+    ADD_FAILURE();
+}
+
+TEST(ExprCompilerTest, can_compile_unary_minus)
+{
+    ADD_FAILURE();
+}
+
+TEST(ExprCompilerTest, can_compile_multiplication)
+{
+    ADD_FAILURE();
+}
+
+TEST(ExprCompilerTest, can_compile_power)
+{
+    ADD_FAILURE();
+}
+
+TEST(ExprCompilerTest, can_compile_calc)
+{
+    ADD_FAILURE();
+}
+
+TEST(ExprCompilerTest, can_compile_derivative)
+{
+    ADD_FAILURE();
+}
+
+TEST(ExprCompilerTest, can_compile_integral)
+{
+    ADD_FAILURE();
+}
+
+TEST(ExprCompilerTest, can_compile_assignment)
+{
+    ADD_FAILURE();
+}
+
+TEST(ExprCompilerTest, can_compile_expression_with_parenthesis)
+{
+    ADD_FAILURE();
+}
+
+TEST(ExprCompilerTest, can_handle_operator_precedence)
+{
+    ADD_FAILURE();
+}
+
+TEST(ExprCompilerTest, can_perform_complex_calculation)
+{
+    ADD_FAILURE();
+}
+
+TEST(ExprCompilerTest, can_use_polynomial_names_in_expr)
+{
+    ADD_FAILURE();
+}
+
+TEST(ExprCompilerTest, exception_on_unclosed_parenthesis)
+{
+    ADD_FAILURE();
+}
+
+TEST(ExprCompilerTest, exception_on_too_many_parentheses)
+{
+    ADD_FAILURE();
+}
+
+TEST(ExprCompilerTest, exception_on_empty_parentheses)
+{
+    ADD_FAILURE();
+}
+
+TEST(ExprCompilerTest, exception_on_two_operators_in_a_row)
+{
+    ADD_FAILURE();
+}
+
+TEST(ExprCompilerTest, exception_on_binary_operator_without_left_argument)
+{
+    ADD_FAILURE();
+}
+
+TEST(ExprCompilerTest, exception_on_binary_operator_without_right_argument)
+{
+    ADD_FAILURE();
+}
+
+TEST(ExprCompilerTest, exception_on_function_use_without_parentheses)
+{
+    ADD_FAILURE();
+}
+
+TEST(ExprCompilerTest, exception_on_function_too_many_params)
+{
+    ADD_FAILURE();
+}
+
+TEST(ExprCompilerTest, exception_on_function_not_enough_params)
+{
+    ADD_FAILURE();
+}
---
+++ test/test_interpreter.cpp
@@ -0,0 +1,50 @@
+#include <gtest/gtest.h>
+#include "expression_interpreter/expression_interpreter.h"
+
+TEST(ExprInterpreterTest, can_execute_single_monomial) {
+    ADD_FAILURE();
+}
+
+TEST(ExprInterpreterTest, can_execute_polynomial_name) {
+    ADD_FAILURE();
+}
+
+TEST(ExprInterpreterTest, can_execute_sum) {
+    ADD_FAILURE();
+}
+
+TEST(ExprInterpreterTest, can_execute_subtraction) {
+    ADD_FAILURE();
+}
+
+TEST(ExprInterpreterTest, can_execute_multiplication) {
+    ADD_FAILURE();
+}
+
+TEST(ExprInterpreterTest, can_execute_power) {
+    ADD_FAILURE();
+}
+
+TEST(ExprInterpreterTest, can_execute_assignment) {
+    ADD_FAILURE();
+}
+
+TEST(ExprInterpreterTest, can_execute_calc) {
+    ADD_FAILURE();
+}
+
+TEST(ExprInterpreterTest, can_execute_der) {
+    ADD_FAILURE();
+}
+
+TEST(ExprInterpreterTest, can_execute_int) {
+    ADD_FAILURE();
+}
+
+TEST(ExprInterpreterTest, can_execute_big_expression) {
+    ADD_FAILURE();
+}
+
+TEST(ExprInterpreterTest, throw_on_unknown_polynomial) {
+    ADD_FAILURE();
+}
